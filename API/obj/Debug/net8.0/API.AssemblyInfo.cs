7cb46680ec0a1b2f768c8c62cf825ece355bec3f
Brandon M Smith <brandon.smith@halocare.co.za>
2024-10-11 11:54:30 +0200

    001: Added empty AccountController

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aea84d82fa06c6e74ada73f93caef27ba6e9ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79036335d62b9d64a5a7ce3c24c978b5d2ab8f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
