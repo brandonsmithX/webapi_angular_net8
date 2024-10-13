66f63e6dec69a5bf139a5ecb1c19ec4f91e8d33f
DevNoneStop <Devnonestop@gmail.com>
2024-10-13 07:15:45 +0200

    sync

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a0db76b8d9affd24b9be87b4ed34e0f387dbba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aea84d82fa06c6e74ada73f93caef27ba6e9ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
