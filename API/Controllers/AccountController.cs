d72613965c56a6019c38117eeefd7307166efca0
Brandon M Smith <brandon.smith@halocare.co.za>
2024-10-17 07:24:06 +0200

    prj-05: Sync

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+
+        public AccountController(UserManager<AppUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
+        )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+
+        }
+
+
+
+    }
+}
