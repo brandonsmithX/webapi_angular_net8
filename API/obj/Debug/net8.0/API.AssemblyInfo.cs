d7eca74a98f4c3252a2a237edcbf77e38551232a
Brandon M Smith <brandon.smith@halocare.co.za>
2024-10-17 07:19:32 +0200

    prj-001

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf68719e90d31da03a5ffd47811436414a5d6e86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e312351031cc2381858f7c477e5787dcdeced56e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
