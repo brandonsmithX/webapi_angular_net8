91553016b0ed2218ae4322d72b8155df5426761a
Brandon M Smith <brandon.smith@halocare.co.za>
2024-10-17 07:26:08 +0200

    PRJ-05: Sync

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55048181cdac4ebf4aa2d7e6d965830b4bd9da0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72613965c56a6019c38117eeefd7307166efca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
